3808d76ccbb1a7613fa9802b3cb2a690f97566e6
hmustain <hunter@bunkerbranding.com>
2023-01-18 13:31:30 -0600

    updated get route for all tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,10 +3,23 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get("/", async (req, res) => {
+  try {
+    // find all categories
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          as: "products",
+        },
+      ],
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to retrieve tags" });
+  }
 });
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
