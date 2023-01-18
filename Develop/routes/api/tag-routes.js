53a1dd4efae8aae88128e02a26dc462d6f1d1d54
hmustain <hunter@bunkerbranding.com>
2023-01-18 13:33:56 -0600

    updated get tag by id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -21,9 +21,25 @@ router.get("/", async (req, res) => {
 });
 
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get("/:id", async (req, res) => {
+  try {
+    // find one category by its primary key
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          as: "products",
+        },
+      ],
+    });
+    if (!tag) {
+      res.status(404).json({ message: "Tag not found" });
+    } else {
+      res.json(tag);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to retrieve Tag" });
+  }
 });
 
 router.post('/', (req, res) => {
