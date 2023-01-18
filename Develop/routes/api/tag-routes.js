3d2e1eac42fced0782e3703ac48d367a1c5a0d40
hmustain <hunter@bunkerbranding.com>
2023-01-18 13:39:03 -0600

    updated post and put methods for tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -42,13 +42,33 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post("/", async (req, res) => {
+  try {
+    // create a new category
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create Tag" });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put("/:id", async (req, res) => {
+  // update category by id value
+  try {
+    const updateTag = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updateTag);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to update tag" });
+  }
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
