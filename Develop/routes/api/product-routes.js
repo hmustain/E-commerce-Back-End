fbd9a0b8f20999263625c736a4efe53ce397c3f7
hmustain <hunter@bunkerbranding.com>
2023-01-18 11:35:13 -0600

    finished the get request for view product by single id in the product.js folder

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -17,11 +17,22 @@ router.get('/', async (req, res) => {
 });
 
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+      // find a single product by its `id`
+      const product = await Product.findByPk(req.params.id, {
+          include: [{ model: Category }, { model: Tag }]
+      });
+      if (!product) {
+          res.status(404).json({ message: 'Product not found' });
+      } else {
+          res.status(200).json(product);
+      }
+  } catch (err) {
+      res.status(500).json({ message: 'Failed to retrieve product' });
+  }
 });
+
 
 // create new product
 router.post('/', (req, res) => {
