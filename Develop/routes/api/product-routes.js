a12711b1650ba01d3d4cf474f7c1654ccc8b28e8
hmustain <hunter@bunkerbranding.com>
2023-01-18 11:31:31 -0600

    updated the get method for all products in the products.js folder

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,10 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+      // find all products
+      const products = await Product.findAll({
+          include: [{ model: Category }, { model: Tag }]
+      });
+      res.status(200).json(products);
+  } catch (err) {
+      res.status(500).json({ message: 'Failed to retrieve products' });
+  }
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
