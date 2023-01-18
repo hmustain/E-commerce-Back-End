c13dba6702a450ecf6532d09b90d53e60920347a
hmustain <hunter@bunkerbranding.com>
2023-01-18 10:57:33 -0600

    added a post request for category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -39,9 +39,16 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+      // create a new category
+      const newCategory = await Category.create(req.body);
+      res.status(200).json(newCategory);
+  } catch (err) {
+      res.status(500).json({ message: 'Failed to create category' });
+  }
 });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
