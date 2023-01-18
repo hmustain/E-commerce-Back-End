3936da9d033a00220bf9d843967912a3b9df5de6
hmustain <hunter@bunkerbranding.com>
2023-01-18 10:43:45 -0600

    updated get routes on the category-routes.js modal

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,15 +3,41 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+      // find all categories
+      const categories = await Category.findAll({
+          include: [{
+              model: Product,
+              as: 'products'
+          }]
+      });
+      res.status(200).json(categories);
+  } catch (err) {
+      res.status(500).json({ message: 'Failed to retrieve categories' });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+
+router.get('/:id', async (req, res) => {
+  try {
+      // find one category by its primary key
+      const category = await Category.findByPk(req.params.id, {
+          include: [{
+              model: Product,
+              as: 'products'
+          }]
+      });
+      if (!category) {
+          res.status(404).json({ message: 'Category not found' });
+      } else {
+          res.json(category);
+      }
+  } catch (err) {
+      res.status(500).json({ message: 'Failed to retrieve category' });
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
